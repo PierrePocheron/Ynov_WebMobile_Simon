500ed092ac383a3a857861de3692a17ded27cc92
PierrePocheron <pierre.pocheron@gmail.com>
2022-11-24 10:35:26 +0100

    chore: config VitePwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// vite.config.js / vite.config.ts
+import { VitePWA } from 'vite-plugin-pwa'
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+    })
+  ]
 })
